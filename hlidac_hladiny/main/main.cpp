3de535f730bf2b32dea19bd2a747b9a96ca03817
Kuba Streit <kuba.streit@gmail.com>
2021-12-28 16:56:20 +0100

    Add fmt

--- hlidac_hladiny/main/main.cpp
+++ hlidac_hladiny/main/main.cpp
@@ -1,10 +1,14 @@
+#define Binary_h // Content of this file interfere with fmt
+
 #include "Arduino.h"
+#include <fmt/core.h>
 
 extern "C" void app_main()
 {
     initArduino();
     Serial.begin(115200);
-    Serial.print("\nHlidac hladiny\n");
+    //Serial.print("\nHlidac hladiny\n");
+    fmt::print("\nHlidac hladiny\n");
     for (;;yield()) {
         Serial.print("pokus dnes\n");
         delay(1000);
